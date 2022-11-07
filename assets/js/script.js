edf6d5046f46600251d01b735bb69ad717dcaa44
milonroy26 <101338962+milonroy26@users.noreply.github.com>
2022-11-07 14:15:13 +0600

    many update

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,4 +49,29 @@ const activeElemOnScroll = function () {
 }
 
 addEventOnElem(window, "scroll", activeElemOnScroll);
+
+/**
+ * filter functionality
+ */
+
+ const filterBtn = document.querySelectorAll("[data-filter-btn]");
+ const filterItems = document.querySelectorAll("[data-filter]");
+ 
+ let lastClickedBtn = filterBtn[0];
+ 
+ const filter = function () {
+   lastClickedBtn.classList.remove("active");
+   this.classList.add("active");
+   lastClickedBtn = this;
+ 
+   for (let i = 0; i < filterItems.length; i++) {
+     if (filterItems[i].dataset.filter === this.dataset.filterBtn) {
+       filterItems[i].style.display = "block";
+     } else {
+       filterItems[i].style.display = "none";
+     }
+   }
+ }
+ 
+ addEventOnElem(filterBtn, "click", filter);
  
